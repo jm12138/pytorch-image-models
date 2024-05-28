47fa1aea25d0ca0507cee4adb296ea401b5deee8
jm12138 <2286040843@qq.com>
2024-05-28 23:28:53 +0800

    Update train.py
    fix validate

--- train.py
+++ train.py
@@ -885,7 +885,7 @@ def main():
                     validate_loss_fn,
                     args,
                     device=device,
-                    amp_autocast=amp_autocast,
+                    amp_autocast=suppress, # SDAA eval mode is not support AMP.
                 )
 
                 if model_ema is not None and not args.model_ema_force_cpu:
@@ -898,7 +898,7 @@ def main():
                         validate_loss_fn,
                         args,
                         device=device,
-                        amp_autocast=amp_autocast,
+                        amp_autocast=suppress, # SDAA eval mode is not support AMP.
                         log_suffix=' (EMA)',
                     )
                     eval_metrics = ema_eval_metrics
